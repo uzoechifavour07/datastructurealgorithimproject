8667cc354a3e6a92a5c902ed89b7bd51f1f0df07
favour <uzoechifavour07@gmail.com>
2025-11-18 14:24:13 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,75 @@
+class Student:
+  def __init__(self, name, age, GPA, id,):
+      self.name = name 
+      self.age = age
+      self.GPA = GPA
+      self.id = id
+      self.registered_class = []
+      self.completed_class = []
+
+student1 = Student("favour", 21, 4.0, "af22304710")
+student2 = Student("eunice", 20, 3.9, "eu22304710")
+student3 = Student("crispine", 20, 3.8,"cr22304710")
+student4 = Student("chima", 25, 3.5, "ch22304710")
+student5 = Student("alex", 18, 2.0, "al22304710")
+student6 = Student("jay", 26, 2.1, "jy22304710")
+student7 = Student("tom", 17, 3.2, "tm22304710")
+student8 = Student("bradley", 31, 1.1, "ba22304710")
+student9 = Student("vicky", 22, 2.9, "vy22304710")
+student10 = Student("ewo", 26, 3.3, "eo22304710")
+student11 = Student("benjamin", 21, 3.1, "af22304720")
+student12 = Student("mia", 22, 3.9, "af22304721")
+student13 = Student("lucas", 20, 3.3, "af22304722")
+student14 = Student("charlotte", 23, 3.7, "af22304723")
+student15 = Student("henry", 19, 3.5, "af22304724")
+student16 = Student("amelia", 21, 4.0, "af22304725")
+student17 = Student("alexander", 22, 3.6, "af22304726")
+student18 = Student("harper", 20, 3.1, "af22304727")
+student19 = Student("daniel", 24, 3.2, "af22304728")
+student20 = Student("evelyn", 19, 3.9, "af22304729")
+student21 = Student("michael", 21, 3.4, "af22304730")
+student22 = Student("ethan", 22, 3.7, "af22304731")
+student23 = Student("abigail", 20, 2.9, "af22304732")
+student24 = Student("matthew", 21, 3.3, "af22304733")
+student25 = Student("ella", 23, 3.8, "af22304734")
+student26 = Student("samuel", 19, 3.5, "af22304735")
+student27 = Student("scarlett", 22, 4.0, "af22304736")
+student28 = Student("david", 20, 3.6, "af22304737")
+student29 = Student("grace", 24, 3.7, "af22304738")
+student30 = Student("joseph", 21, 2.2, "af22304739")
+student31 = Student("chloe", 19, 3.9, "af22304740")
+student32 = Student("carter", 22, 3.4, "af22304741")
+student33 = Student("victoria", 20, 3.8, "af22304742")
+student34 = Student("john", 23, 1.1, "af22304743")
+student35 = Student("riley", 21, 3.7, "af22304744")
+student36 = Student("owen", 19, 2.5, "af22304745")
+student37 = Student("aubrey", 22, 3.9, "af22304746")
+student38 = Student("jackson", 20, 2.3, "af22304747")
+student39 = Student("zoey", 24, 1.8, "af22304748")
+student40 = Student("sebastian", 21, 3.2, "af22304749")
+
+class Course:
+   def __init__(self, course_code, course_name, capacity):
+          self.course_code = course_code
+          self.course_name = course_name
+          self.capacity = capacity
+          self.enrolled_students = []
+
+course1 = Course("CS101", "Introduction to Programming", 20)
+course1 = Course("CS101", "Introduction to Programming", 20)
+course2 = Course("MATH201", "Calculus I", 25)
+course3 = Course("ENG101", "English Composition", 30)
+course4 = Course("PHY301", "Physics Fundamentals", 22)
+course5 = Course("BIO110", "Biology Basics", 28)
+course6 = Course("CHEM201", "General Chemistry", 24)
+course7 = Course("HIST101", "World History", 35)
+course8 = Course("PSY150", "Introduction to Psychology", 32)
+course9 = Course("ART120", "Art Appreciation", 18)
+course10 = Course("ECON201", "Microeconomics", 26)
+course11 = Course("BUS101", "Business Principles", 29)
+course12 = Course("CS201", "Data Structures", 20)
+course13 = Course("MATH202", "Calculus II", 23)
+course14 = Course("ENG201", "Advanced Writing", 27)
+course15 = Course("PHY302", "Modern Physics", 21)
+
+
